055d5546e8358acb1e4ec306de7df3b2c62a4996
LeandroSantosDasChagas <68669590+LeandroSantosDasChagas@users.noreply.github.com>
2023-05-04 19:31:54 -0300

    Add files via upload

---
+++ sketch_apr20aEXE3.ino
@@ -0,0 +1,18 @@
+// C++ code
+//
+int definir = 0;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  definir = 1000;
+}
+
+void loop()
+{
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, HIGH);
+}
